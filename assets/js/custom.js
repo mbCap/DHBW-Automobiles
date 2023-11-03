6ea667f83d7b09dd1068042f3fe6d4206a193e38
mbCap <magnus.blsm@gmail.com>
2023-11-04 00:26:59 +0100

    latest

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -149,57 +149,6 @@ $(document).ready(function(){
 
 
 // Annahme: Hier würdest du die Daten aus deiner Datenquelle (z.B. XML) abrufen und in die Variablen einfügen
-var brand = "Volkswagen";
-var model = "Golf";
-var b2_1 = "12345";
-var b2_2 = "67890";
-var j = "1234";
-var four = "5678";
-var d1 = "9012";
-var d2 = "3456";
-var two = "7890";
-var five = "1234";
-var v9 = "5678";
-var fourteen = "9012";
-var p3 = "3456";
-var s = "7890";
-var nefz = "12 g/km";
-var urban = "5.5 l/100km";
-var extra_urban = "4.0 l/100km";
-var combined = "4.8 l/100km";
-var co2_combined = "110 g/km";
-var wltp = "14 g/km";
-var fast = "8.5 l/100km";
-var quick = "6.0 l/100km";
-var slow = "7.2 l/100km";
-var co2_combined2 = "95 g/km";
-
-// Aktualisiere die Anzeigetafel mit den Daten
-document.getElementById("brand").textContent = brand;
-document.getElementById("model").textContent = model;
-document.getElementById("b2_1").textContent = b2_1;
-document.getElementById("b2_2").textContent = b2_2;
-document.getElementById("j").textContent = j;
-document.getElementById("four").textContent = four;
-document.getElementById("d1").textContent = d1;
-document.getElementById("d2").textContent = d2;
-document.getElementById("two").textContent = two;
-document.getElementById("five").textContent = five;
-document.getElementById("v9").textContent = v9;
-document.getElementById("fourteen").textContent = fourteen;
-document.getElementById("p3").textContent = p3;
-document.getElementById("s").textContent = s;
-document.getElementById("nefz").textContent = nefz;
-document.getElementById("urban").textContent = urban;
-document.getElementById("extra_urban").textContent = extra_urban;
-document.getElementById("combined").textContent = combined;
-document.getElementById("co2_combined").textContent = co2_combined;
-document.getElementById("wltp").textContent = wltp;
-document.getElementById("fast").textContent = fast;
-document.getElementById("quick").textContent = quick;
-document.getElementById("slow").textContent = slow;
-document.getElementById("co2_combined2").textContent = co2_combined2;
-
 
 
 
