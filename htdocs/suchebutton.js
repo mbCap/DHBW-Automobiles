680af9c006438148dd9659be08a56ae1fbac88ba
Magnus <magnus.blessmann@capgemini.com>
2023-11-08 00:56:28 +0100

    latest

--- htdocs/suchebutton.js
+++ htdocs/suchebutton.js
@@ -15,63 +15,63 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Die Funktion zur Durchführung der Suche
-    function performSearch() {
-        // Holen Sie den eingegebenen Suchbegriff aus dem Input-Feld
-        var searchTerm = searchInput.value;
+function performSearch() {
+    // Holen Sie den eingegebenen Suchbegriff aus dem Input-Feld und konvertieren Sie ihn zu Kleinbuchstaben
+    var searchTerm = searchInput.value.toLowerCase();
 
-        // Überprüfen, ob ein Suchbegriff eingegeben wurde
-        if (searchTerm) {
-            // Hier senden wir eine AJAX-Anfrage, um die automobiles.xml-Datei abzurufen
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "http://localhost/DHBW-Automobiles/htdocs/Backend/automobiles.xml", true);
-            xhr.send();
+    // Überprüfen, ob ein Suchbegriff eingegeben wurde
+    if (searchTerm) {
+        // Hier senden wir eine AJAX-Anfrage, um die automobiles.xml-Datei abzurufen
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost/DHBW-Automobiles/htdocs/Backend/automobiles.xml", true);
+        xhr.send();
 
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        var response = xhr.responseXML; // Die Daten aus der automobiles.xml-Datei
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var response = xhr.responseXML; // Die Daten aus der automobiles.xml-Datei
 
-                        // Verarbeite die XML-Daten und filtere nach dem Suchbegriff
-                        var automobiles = response.getElementsByTagName("automobile");
-                        var searchResults = [];
+                    // Verarbeite die XML-Daten und filtere nach dem Suchbegriff (ignoriert Groß- und Kleinschreibung)
+                    var automobiles = response.getElementsByTagName("automobile");
+                    var searchResults = [];
 
-                        for (var i = 0; i < automobiles.length; i++) {
-                            var automobile = automobiles[i];
-                            var marke = automobile.getElementsByTagName("Marke")[0].textContent;
+                    for (var i = 0; i < automobiles.length; i++) {
+                        var automobile = automobiles[i];
+                        var marke = automobile.getElementsByTagName("Marke")[0].textContent;
+
+                        // Überprüfen, ob die Marke dem Suchbegriff entspricht (ignoriert Groß- und Kleinschreibung)
+                        if (marke.toLowerCase().includes(searchTerm)) {
                             var modell = automobile.getElementsByTagName("model")[0].textContent; // "model" für Modell; keine gute Attributsbezeichnung
-
-                            // Überprüfen, ob die Marke oder das Modell dem Suchbegriff entsprechen
-                            if (marke.includes(searchTerm) || modell.includes(searchTerm)) {
-                                var result = {
-                                    Marke: marke,
-                                    Modell: modell
-                                };
-                                searchResults.push(result);
-                            }
+                            var result = {
+                                Marke: marke,
+                                Modell: modell
+                            };
+                            searchResults.push(result);
                         }
-
-                        // Erzeuge eine HTML-Liste der Suchergebnisse
-                        var htmlString = "";
-                        for (var j = 0; j < searchResults.length; j++) {
-                            var result = searchResults[j];
-                            htmlString += "Marke: " + result.Marke + "<br>";
-                            htmlString += "model: " + result.Modell + "<br>";
-                            htmlString += "<hr>";
-                        }
-
-                        // Wenn keine Übereinstimmungen gefunden wurden
-                        if (htmlString === "") {
-                            htmlString = "Keine Übereinstimmungen gefunden.";
-                        }
-
-                        searchResultElement.innerHTML = htmlString;
-                    } else {
-                        console.error("Fehler beim Abrufen der Daten. Statuscode: " + xhr.status);
                     }
+
+                    // Erzeuge eine HTML-Liste der Suchergebnisse
+                    var htmlString = "";
+                    for (var j = 0; j < searchResults.length; j++) {
+                        var result = searchResults[j];
+                        htmlString += "Marke: " + result.Marke + "<br>";
+                        htmlString += "model: " + result.Modell + "<br>";
+                        htmlString += "<hr>";
+                    }
+
+                    // Wenn keine Übereinstimmungen gefunden wurden
+                    if (htmlString === "") {
+                        htmlString = "Keine Übereinstimmungen gefunden.";
+                    }
+
+                    searchResultElement.innerHTML = htmlString;
+                } else {
+                    console.error("Fehler beim Abrufen der Daten. Statuscode: " + xhr.status);
                 }
-            };
-        } else {
-            searchResultElement.innerHTML = "Bitte geben Sie einen Suchbegriff ein.";
-        }
+            }
+        };
+    } else {
+        searchResultElement.innerHTML = "Bitte geben Sie einen Suchbegriff ein.";
     }
+}
 });
