35a442aaaff96a367e150675e0b5538f6e5a124a
Magnus <magnus.blessmann@capgemini.com>
2023-11-08 00:48:27 +0100

    latest

--- htdocs/suchebutton.js
+++ htdocs/suchebutton.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     // Wir wählen das Such-Input-Feld und den Suchen-Button aus
     var searchInput = document.getElementById("search-input");
     var searchButton = document.getElementById("search-button");
+    var searchResultElement = document.getElementById("search-result");
 
     // Wir fügen einen Event-Listener für den Klick auf den Suchen-Button hinzu
     searchButton.addEventListener("click", performSearch);
 
-    // Wir fügen einen Event-Listener für die Enter-Taste auf dem Such-Input-Feld hinzu
+    // Wir fügen einen Event-Listener hinzu, um die Suche bei Drücken der "Enter"-Taste auszulösen
     searchInput.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
             performSearch();
@@ -20,72 +21,57 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Überprüfen, ob ein Suchbegriff eingegeben wurde
         if (searchTerm) {
-            // Hier senden wir eine AJAX-Anfrage, um Daten im JSON-Format von anbindungausgabe.php abzurufen
+            // Hier senden wir eine AJAX-Anfrage, um die automobiles.xml-Datei abzurufen
             var xhr = new XMLHttpRequest();
-            xhr.open("GET", "anbindungausgabecopy.php" + searchTerm, true);
+            xhr.open("GET", "http://localhost/DHBW-Automobiles/htdocs/Backend/automobiles.xml", true);
             xhr.send();
 
             xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    var response = xhr.responseText; // Die Daten von anbindungausgabe.php im JSON-Format
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var response = xhr.responseXML; // Die Daten aus der automobiles.xml-Datei
 
-                    // Konvertiere die JSON-Antwort in ein JavaScript-Objekt
-                    var jsonData = JSON.parse(response);
+                        // Verarbeite die XML-Daten und filtere nach dem Suchbegriff
+                        var automobiles = response.getElementsByTagName("automobile");
+                        var searchResults = [];
 
-                    // Verarbeite die JSON-Daten
-                    console.log("JSON-Daten von anbindungausgabesuche.php:", jsonData);
+                        for (var i = 0; i < automobiles.length; i++) {
+                            var automobile = automobiles[i];
+                            var marke = automobile.getElementsByTagName("Marke")[0].textContent;
+                            var modell = automobile.getElementsByTagName("model")[0].textContent; // "model" für Modell; keine gute Attributsbezeichnung
 
-                    // Du kannst jetzt die jsonData in deinem Code verwenden, um sie anzuzeigen oder weiter zu verarbeiten
+                            // Überprüfen, ob die Marke oder das Modell dem Suchbegriff entsprechen
+                            if (marke.includes(searchTerm) || modell.includes(searchTerm)) {
+                                var result = {
+                                    Marke: marke,
+                                    Modell: modell
+                                };
+                                searchResults.push(result);
+                            }
+                        }
+
+                        // Erzeuge eine HTML-Liste der Suchergebnisse
+                        var htmlString = "";
+                        for (var j = 0; j < searchResults.length; j++) {
+                            var result = searchResults[j];
+                            htmlString += "Marke: " + result.Marke + "<br>";
+                            htmlString += "model: " + result.Modell + "<br>";
+                            htmlString += "<hr>";
+                        }
+
+                        // Wenn keine Übereinstimmungen gefunden wurden
+                        if (htmlString === "") {
+                            htmlString = "Keine Übereinstimmungen gefunden.";
+                        }
+
+                        searchResultElement.innerHTML = htmlString;
+                    } else {
+                        console.error("Fehler beim Abrufen der Daten. Statuscode: " + xhr.status);
+                    }
                 }
-            }
+            };
         } else {
-            var searchResultElement = document.getElementById("search-result");
             searchResultElement.innerHTML = "Bitte geben Sie einen Suchbegriff ein.";
         }
     }
 });
-
-
-/*document.addEventListener("DOMContentLoaded", function() {
-    // Wir wählen das Such-Input-Feld und den Suchen-Button aus
-    var searchInput = document.getElementById("search-input");
-    var searchButton = document.getElementById("search-button");
-
-    // Wir fügen einen Event-Listener für den Klick auf den Suchen-Button hinzu
-    searchButton.addEventListener("click", performSearch);
-
-    // Wir fügen einen Event-Listener für die Enter-Taste auf dem Such-Input-Feld hinzu
-    searchInput.addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            performSearch();
-        }
-    });
-
-    // Die Funktion zur Durchführung der Suche
-    function performSearch() {
-        // Holen Sie den eingegebenen Suchbegriff aus dem Input-Feld
-        var searchTerm = searchInput.value;
-
-        // Überprüfen, ob ein Suchbegriff eingegeben wurde
-        if (searchTerm) {
-            // Hier senden wir eine AJAX-Anfrage, um Daten aus anbindungausgabe.php abzurufen
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "anbindungausgabe.php", true);
-            xhr.send();
-
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    var response = xhr.responseText; // Die Daten von anbindungausgabe.php
-                    console.log("Antwort von anbindungausgabe.php: " + response);
-
-                    // Die Daten in das HTML-Element einfügen
-                    var searchResultElement = document.getElementById("search-result");
-                    searchResultElement.innerHTML = response;
-                }
-            }
-        } else {
-            var searchResultElement = document.getElementById("search-result");
-            searchResultElement.innerHTML = "Bitte geben Sie einen Suchbegriff ein.";
-        }
-    }
-});*/
