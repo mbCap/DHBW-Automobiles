cbdabec232cc3f7a7987c9ce6a21ecc6da1b74e5
Vlexx <a.gehring853@gmail.com>
2023-11-09 18:59:31 +0100

    Filter Button + befuellen richtig

--- htdocs/FilterBefuellen.js
+++ htdocs/FilterBefuellen.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   var xmlDoc; // Variable für den XML-Content
-
+ 
   // Funktion zum Laden der XML-Datei und Füllen der Dropdown-Listen
   function loadXML() {
     var xhttp = new XMLHttpRequest();
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
     xhttp.open("GET", "http://localhost/DHBW-Automobiles/htdocs/Backend/automobiles.xml", true);
     xhttp.send();
   }
-
+ 
   // Funktion zum Verarbeiten der XML-Daten und Füllen der Dropdown-Listen
   function parseXML() {
     // Funktion zum Füllen der Dropdown-Liste mit eindeutigen und alphabetisch sortierten Werten aus der XML
@@ -21,81 +21,85 @@ document.addEventListener("DOMContentLoaded", function() {
       var select = document.getElementById(id);
       var options = xmlDoc.getElementsByTagName(tagName);
       var uniqueValues = ["auswählen"];
-
+ 
       for (var i = 0; i < options.length; i++) {
         var option = options[i].textContent;
         if (!uniqueValues.includes(option)) {
           uniqueValues.push(option);
         }
       }
-
+ 
       // Alphabetisch sortieren
       uniqueValues.sort();
-
+ 
       for (var i = 0; i < uniqueValues.length; i++) {
         var optionElement = document.createElement("option");
         optionElement.value = uniqueValues[i];
         optionElement.textContent = uniqueValues[i];
         select.appendChild(optionElement);
       }
-
+ 
       return uniqueValues;
     }
-
+ 
     // Fülle die Dropdown-Listen und speichere die Werte in Arrays
     var fahrzeugklasseOptions = fillFilterDropdown("fahrzeugklasse-filter", "Fahrzeugklasse");
     var kraftstoffverbrauchOptions = fillFilterDropdown("kraftstoffverbrauch-filter", "kombiniert");
-    var markeOptions = fillFilterDropdown("marke-filter", "Marke");
+    var MarkeOptions = fillFilterDropdown("marke-filter", "Marke");
     var schadstoffklasseOptions = fillFilterDropdown("schadstoffklasse-filter", "Schadstoffklasse");
     var modellOptions = fillFilterDropdown("modell-filter", "model");
     var kraftstoffOptions = fillFilterDropdown("kraftstoff-filter", "Kraftstoff");
-
-
-
+ 
+ 
+ 
     //Eventlistener für verschiedene Filter
-
+ 
     // Eventlistener für Fahrzeugklasse
-    document.getElementById("fahrzeugklasse-filter").addEventListener("change", function() {
+   /*  document.getElementById("fahrzeugklasse-filter").addEventListener("change", function() {
       var selectedFahrzeugklasse = this.value;
-      filterMarkeOptionsByFahrzeugklasse(selectedFahrzeugklasse);
-      filterModellOptionsByFahrzeugklasse(selectedFahrzeugklasse);
+      filtermarkeOptionsByFahrzeugklasse(selectedFahrzeugklasse);
+      filtermodellOptionsByFahrzeugklasse(selectedFahrzeugklasse);
+      filterkrafstoffverbrauchOptionsByFahrzeugklasse(selectedFahrzeugklasse);
+      filterschadstoffklasseOptionsByFahrzeugklasse(selectedFahrzeugklasse);
+      filterkraftstoffOptionsByFahrzeugklasse(selectedFahrzeugklasse);
     });
-
-    // Event-Listener für Marke hinzu
+ 
+     // Event-Listener für Marke
     document.getElementById("marke-filter").addEventListener("change", function() {
         var selectedMarke = this.value;
         filterModellOptions(selectedMarke);
-    });
-
-
-
+        filterFahrzeugklasseOptions(selectedMarke);
+    });  
+*/
+ 
+ 
     //Filterfunktionen
-
+ 
     // Filterfunktion für Marke basierend auf auswählter Fahrzeugklasse
-    function filterMarkeOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
+/*     function filtermarkeOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
       var select = document.getElementById("marke-filter");
       select.innerHTML = ""; // Dropdown-Liste leeren
-
-      for (var i = 0; i < markeOptions.length; i++) {
-        var marke = markeOptions[i];
-        if (marke === "auswählen" || getFahrzeugklasseFromMarke(marke) === selectedFahrzeugklasse) {
+ 
+      for (var i = 0; i < MarkeOptions.length; i++) {
+        var Marke = MarkeOptions[i];
+        if (Marke === "auswählen" || getFahrzeugklasseFromMarke(Marke) === selectedFahrzeugklasse) {
           var optionElement = document.createElement("option");
-          optionElement.value = marke;
-          optionElement.textContent = marke;
+          optionElement.value = Marke;
+          optionElement.textContent = Marke;
           select.appendChild(optionElement);
         }
       }
       select.value = "auswählen";
     }
-
+ 
      // Filterfunktion für Modell basierend auf auswählter Fahrzeugklasse
-     function filterModellOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
+     function filtermodellOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
       var select = document.getElementById("modell-filter");
       select.innerHTML = ""; // Dropdown-Liste leeren
-
+ 
       for (var i = 0; i < modellOptions.length; i++) {
         var modell = modellOptions[i];
-        if (modell === "auswählen" || getFahrzeugklasseFrommodell(modell) === selectedFahrzeugklasse) {
+        if (modell === "auswählen" || getFahrzeugklasseFromModel(modell) === selectedFahrzeugklasse) {
           var optionElement = document.createElement("option");
           optionElement.value = modell;
           optionElement.textContent = modell;
@@ -104,13 +108,66 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       select.value = "auswählen";
     }
-
-
+ 
+    // Filterfunktion für durchschnittlicher Kraftstoffverbrauch basierend auf auswählter Fahrzeugklasse
+    function filterkrafstoffverbrauchOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
+      var select = document.getElementById("kraftstoffverbrauch-filter");
+      select.innerHTML = ""; // Dropdown-Liste leeren
+ 
+      for (var i = 0; i < kraftstoffverbrauchOptions.length; i++) {
+        var kraftstoffverbrauch = kraftstoffverbrauchOptions[i];
+        if (kraftstoffverbrauch === "auswählen" || getFahrzeugklasseFromkraftstoffverbrauch(kraftstoffverbrauch) === selectedFahrzeugklasse) {
+          var optionElement = document.createElement("option");
+          optionElement.value = kraftstoffverbrauch;
+          optionElement.textContent = kraftstoffverbrauch;
+          select.appendChild(optionElement);
+        }
+      }
+      select.value = "auswählen";
+    }
+ 
+    // Filterfunktion für Schadstoffklasse basierend auf auswählter Fahrzeugklasse
+    function filterschadstoffklasseOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
+      var select = document.getElementById("schadstoffklasse-filter");
+      select.innerHTML = ""; // Dropdown-Liste leeren
+ 
+      for (var i = 0; i < schadstoffklasseOptions.length; i++) {
+        var schadstoffklasse = schadstoffklasseOptions[i];
+ 
+        if (schadstoffklasse === "auswählen" || getFahrzeugklasseFromschadstoffklasse(schadstoffklasse) === selectedFahrzeugklasse) {
+          var optionElement = document.createElement("option");
+          optionElement.value = schadstoffklasse;
+          optionElement.textContent = schadstoffklasse;
+          select.appendChild(optionElement);
+        }
+      }
+      select.value = "auswählen";
+    }
+ 
+    // Filterfunktion für Kraftstoff basierend auf auswählter Fahrzeugklasse
+    function filterkraftstoffOptionsByFahrzeugklasse(selectedFahrzeugklasse) {
+      var select = document.getElementById("kraftstoff-filter");
+      select.innerHTML = ""; // Dropdown-Liste leeren
+ 
+      for (var i = 0; i < kraftstoffOptions.length; i++) {
+        var kraftstoff = kraftstoffOptions[i];
+ 
+        if (kraftstoff === "auswählen" || getFahrzeugklasseFromkraftstoff(kraftstoff) === selectedFahrzeugklasse) {
+          var optionElement = document.createElement("option");
+          optionElement.value = kraftstoff;
+          optionElement.textContent = kraftstoff;
+          select.appendChild(optionElement);
+        }
+      }
+      select.value = "auswählen";
+    }
+ 
+ 
     // Filterfunktion für Modell-Optionen basierend auf der ausgewählten Marke
     function filterModellOptions(selectedMarke) {
     var select = document.getElementById("modell-filter");
     select.innerHTML = ""; // Dropdown-Liste leeren
-
+ 
       for (var i = 0; i < modellOptions.length; i++) {
         var modell = modellOptions[i];
         if (modell === "auswählen" || getMarkeFromModel(modell) === selectedMarke) {
@@ -122,48 +179,89 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       select.value = "auswählen";
     }
-
-
+ 
+     // Filterfunktion für Fahrzeugklasse basierend auf der ausgewählten Marke
+     function filterFahrzeugklasseOptions(selectedMarke) {
+      var select = document.getElementById("fahrzeugklasse-filter");
+      select.innerHTML = ""; // Dropdown-Liste leeren
+  
+        for (var i = 0; i < fahrzeugklasseOptions.length; i++) {
+          var fahrzeugklasse = fahrzeugklasseOptions[i];
+          if (modell === "auswählen" || getMarkeFromFahrzeugklasse(fahrzeugklasse) === selectedMarke) {
+            var optionElement = document.createElement("option");
+            optionElement.value = fahrzeugklasse;
+            optionElement.textContent = fahrzeugklasse;
+            select.appendChild(optionElement);
+          }
+        }
+        select.value = "auswählen";
+      }  */
+ 
+ 
     //Ermittlung der Daten aus der XML-Datei
-
-    // Funktion zur Ermittlung der Marke eines Modells aus den XML-Daten
-    function getMarkeFromModel(model) {
+ 
+    // Funktion zur Ermittlung der Fahrzeugklasse einer Marke aus den XML-Daten
+  /*   function getFahrzeugklasseFromMarke(Marke) {
+      var nodes = xmlDoc.getElementsByTagName("Marke");
+      for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].textContent === Marke) {
+          var parent = nodes[i].parentNode;
+          return parent.getElementsByTagName("Fahrzeugklasse")[0].textContent;
+        }
+      }
+      return null;
+    }
+ 
+    // Funktion zur Ermittlung der Fahrzeugklasse eines Modells aus den XML-Daten
+    function getFahrzeugklasseFromModel(model) {
       var nodes = xmlDoc.getElementsByTagName("model");
       for (var i = 0; i < nodes.length; i++) {
         if (nodes[i].textContent === model) {
           var parent = nodes[i].parentNode;
-          return parent.getElementsByTagName("Marke")[0].textContent;
+          return parent.getElementsByTagName("Fahrzeugklasse")[0].textContent;
         }
       }
       return null;
     }
-
-    // Funktion zur Ermittlung der Fahrzeugklasse einer Marke aus den XML-Daten
-    function getFahrzeugklasseFromMarke(marke) {
-      var nodes = xmlDoc.getElementsByTagName("Marke");
+ 
+    // Funktion zur Ermittlung der Fahrzeugklasse des durchschnittlichen Kraftstoffverbrauchs aus den XML-Daten
+    function getFahrzeugklasseFromkraftstoffverbrauch(kraftstoffverbrauch) {
+      var nodes = xmlDoc.getElementsByTagName("kombiniert");
       for (var i = 0; i < nodes.length; i++) {
-        if (nodes[i].textContent === marke) {
+        if (nodes[i].textContent === kraftstoffverbrauch) {
           var parent = nodes[i].parentNode;
           return parent.getElementsByTagName("Fahrzeugklasse")[0].textContent;
         }
       }
       return null;
     }
-
-    // Funktion zur Ermittlung der Fahrzeugklasse eines Modells aus den XML-Daten
-    function getFahrzeugklasseFrommodell(modell) {
-      var nodes = xmlDoc.getElementsByTagName("model");
+ 
+    // Funktion zur Ermittlung der Fahrzeugklasse des Schadstoffklasse aus den XML-Daten
+    function getFahrzeugklasseFromschadstoffklasse(schadstoffklasse) {
+      var nodes = xmlDoc.getElementsByTagName("Schadstoffklasse");
       for (var i = 0; i < nodes.length; i++) {
-        if (nodes[i].textContent === modell) {
+        if (nodes[i].textContent === schadstoffklasse) {
           var parent = nodes[i].parentNode;
           return parent.getElementsByTagName("Fahrzeugklasse")[0].textContent;
         }
       }
       return null;
     }
-
-
-   // Funktion zur Ermittlung der Marke eines Modells aus den XML-Daten
+ 
+    // Funktion zur Ermittlung der Fahrzeugklasse des Krafstoffs aus den XML-Daten
+    function getFahrzeugklasseFromkraftstoff(kraftstoff) {
+      var nodes = xmlDoc.getElementsByTagName("Kraftstoff");
+      for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].textContent === kraftstoff) {
+          var parent = nodes[i].parentNode;
+          return parent.getElementsByTagName("Fahrzeugklasse")[0].textContent;
+        }
+      }
+      return null;
+    }
+ 
+ 
+     // Funktion zur Ermittlung der Marke eines Modells aus den XML-Daten
     function getMarkeFromModel(model) {
       var nodes = xmlDoc.getElementsByTagName("model");
       for (var i = 0; i < nodes.length; i++) {
@@ -174,11 +272,30 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         return null;
       }
-	  }
+    
+ 
+    // Funktion zur Ermittlung der Marke eines Fahrzeugklasse aus den XML-Daten
+    function getMarkeFromFahrzeugklasse(fahrzeugklasse) {
+      var nodes = xmlDoc.getElementsByTagName("Fahrzeugklasse");
+      for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].textContent === fahrzeugklasse) {
+          var parent = nodes[i].parentNode;
+          return parent.getElementsByTagName("Marke")[0].textContent;
+          }
+        }
+        return null;
+      } */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
   
-  
-  
-
   // Die XML-Datei laden und die Dropdown-Listen füllen, wenn das Dokument geladen ist
   loadXML();
 });
+ 
--- htdocs/filterbtn.js
+++ htdocs/filterbtn.js
@@ -1,6 +1,6 @@
 // Warten, bis das Dokument geladen ist, dann die Funktion ausführen
 document.addEventListener("DOMContentLoaded", function() {
-
+ 
     // Input der Filterfelder in eine Variable schreiben
     var fahrzeugklasseInput = document.getElementById("fahrzeugklasse-filter");
     var markeInput = document.getElementById("marke-filter");
@@ -8,15 +8,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var kraftstoffverbrauchInput = document.getElementById("kraftstoffverbrauch-filter");
     var schadstoffklasseInput = document.getElementById("schadstoffklasse-filter");
     var kraftstoffInput = document.getElementById("kraftstoff-filter");
-
+ 
     // Filterbutton
-    var filterButton = document.getElementById("filter-btn"); 
+    var filterButton = document.getElementById("filter-btn");
     // Definieren, wo die Ergebnisse ausgegeben werden sollen
     var filterResultElement = document.getElementById("output");
-
+ 
     // Eventlistener auf Filter Button
     filterButton.addEventListener("click", performSearch);
-
+ 
     // Methode zur Durchführung der Suche
     function performSearch() {
         var selectedFahrzeugklasse = fahrzeugklasseInput.value.toLowerCase();
@@ -25,31 +25,31 @@ document.addEventListener("DOMContentLoaded", function() {
         var selectedKraftstoffverbrauch = kraftstoffverbrauchInput.value.toLowerCase();
         var selectedSchadstoffklasse = schadstoffklasseInput.value.toLowerCase();
         var selectedKraftstoff = kraftstoffInput.value.toLowerCase();
-
+ 
         // AJAX Call um XML einzulesen
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "http://localhost/DHBW-Automobiles/htdocs/Backend/automobiles.xml", true);
         xhr.send();
-
+ 
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     var response = xhr.responseXML; // Die Daten aus der automobiles.xml-Datei
-
+ 
                     // Verarbeite die XML-Daten und filtere
                     var automobiles = response.getElementsByTagName("automobile");
                     var filterResults = [];
-
+ 
                     for (var i = 0; i < automobiles.length; i++) {
                         var automobile = automobiles[i];
-
+ 
                         var marke = automobile.getElementsByTagName("Marke")[0].textContent;
                         var modell = automobile.getElementsByTagName("model")[0].textContent;
                         var fahrzeugklasse = automobile.getElementsByTagName("Fahrzeugklasse")[0].textContent;
                         var kraftstoffverbrauch = automobile.getElementsByTagName("kombiniert")[0].textContent;
                         var schadstoffklasse = automobile.getElementsByTagName("Schadstoffklasse")[0].textContent;
                         var kraftstoff = automobile.getElementsByTagName("Kraftstoff")[0].textContent;
-
+ 
                         if (
                             (selectedMarke === "default" || marke.toLowerCase().includes(selectedMarke)) &&
                             (selectedModell === "default" || modell.toLowerCase().includes(selectedModell)) &&
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         }
                     }
                     //console.log("Anzahl der Filterergebnisse:", filterResults.length);
-
+ 
                     // Erzeuge eine HTML-Liste der Suchergebnisse
                     var htmlString = "";
                     for (var j = 0; j < filterResults.length; j++) {
@@ -84,12 +84,12 @@ document.addEventListener("DOMContentLoaded", function() {
                         // Hier weitere Eigenschaften hinzufügen
                         htmlString += "<hr>";
                     }
-
+ 
                     // Wenn keine Übereinstimmungen gefunden wurden
                     if (htmlString === "") {
                         htmlString = "Keine Übereinstimmungen gefunden.";
                     }
-
+ 
                     filterResultElement.innerHTML = htmlString;
                 } else {
                     console.error("Fehler beim Abrufen der Daten. Statuscode: " + xhr.status);
